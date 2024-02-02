be4280899989b98637fec5a1d253baa36eafa183
Juan Millan <juanes14569@gmail.com>
2024-02-02 11:29:42 +0100

    Refactor Focus Event handler

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"context"
-    "fmt"
-    "github.com/joshuarubin/go-sway"
-    "github.com/millancore/selva"
+	"fmt"
 	"log"
+
+	"github.com/joshuarubin/go-sway"
+	"github.com/millancore/selva"
 )
 
 type eventHandler struct {
@@ -14,42 +15,53 @@ type eventHandler struct {
 }
 
 func (handler eventHandler) Window(ctx context.Context, ev sway.WindowEvent) {
-    client := handler.Client
+	client := handler.Client
 
-    rootNode, err := client.Sway.GetTree(ctx)
-    if err != nil {
-        log.Fatal(err)
-    }
+	rootNode, err := client.Sway.GetTree(ctx)
 
-    if ev.Change == "title" {
-        return
-    }
+	ws := selva.GetFocusedWorkspace(ctx, client)
 
-    if ev.Change == "urgent" {
-        client.Sway.RunCommand(ctx, fmt.Sprintf("[con_id=%d] focus", ev.Container.ID))
-    }
+	if client.Config.SkipWorkspaces != nil {
+		for _, wsNum := range client.Config.SkipWorkspaces {
+			if ws.Num == wsNum {
+				return
+			}
+		}
+	}
 
+	if err != nil {
+		log.Fatal(err)
+	}
 
-//    fmt.Println(ev.Change)
-    selva.AutoResize(ctx, client, &selva.Node{Node: rootNode})
+	if ev.Change == "title" {
+		return
+	}
+
+	if ev.Change == "urgent" {
+		client.Sway.RunCommand(ctx, fmt.Sprintf("[con_id=%d] focus", ev.Container.ID))
+	}
+
+	// fmt.Println(ev.Change)
+	selva.AutoResize(ctx, client, ws, rootNode.FocusedNode())
 }
 
 func main() {
 
 	ctx := context.Background()
 
-    swayClient, err := sway.New(ctx)
-    if err != nil {
-        log.Fatal(err)
-    }
+	swayClient, err := sway.New(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    client := selva.Client{
-        Sway: swayClient,
-        Config: selva.Config{
-           OuputWidth: 2536,
-            FocusWidth: 1500,
-        },
-    }
+	client := selva.Client{
+		Sway: swayClient,
+		Config: selva.Config{
+			OuputWidth:     2536,
+			FocusWidth:     1500,
+			SkipWorkspaces: []int{3, 6},
+		},
+	}
 
 	handler := eventHandler{
 		EventHandler: sway.NoOpEventHandler(),
